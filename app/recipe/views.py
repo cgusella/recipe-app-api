ce498380a3e280827399195a2f0b781512bb6cfb
Cristian Gusella <cristian.gusella@servizist.it>
2022-04-19 16:26:52 +0200

    Implement of listing recipes: test, serializers, views, urls.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 from core.models import Ingredient
+from core.models import Recipe
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -33,3 +34,15 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingridient in database"""
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage Recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('id')
